28f5ff3ff302cbb3e005f21dda2eafc9bccb62eb
Prabeen04 <prabeen.strange@gmail.com>
2018-06-24 21:00:25 +0530

    graphql sschema created

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
+
+var schema = buildSchema(
+    `
+    type Query{
+        hello: String
+    }
+    `
+);
